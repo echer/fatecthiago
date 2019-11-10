97e5fd3a426f0d2bb3af93bf61863a856662dbc6
alan echer <alan.echer@gmail.com>
2019-11-10 19:53:45 -0300

    primeiro commit

---
+++ src/test/java/com/fatec/projetoweb/ProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.projetoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
